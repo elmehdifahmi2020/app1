5a28a3f6263565b5a1ebca8b26b7797e9afcbf31
elmehdifahmi2020 <elmehdifahmi2020@gmail.com>
2023-11-09 18:10:00 +0100

    app

---
+++ app/src/main/java/com/example/interface_gr/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.interface_gr
+
+import android.content.Context
+import android.os.Bundle
+import android.view.GestureDetector
+import android.view.GestureDetector.OnGestureListener
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
